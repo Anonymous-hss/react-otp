090a70e1d90363e7eaf806d17980a6d48842297b
Anonymous-hss <harshalsawatkar24@gmail.com>
2024-07-08 16:18:30 +0530

    first commit

---
+++ src/Component/Opt/opt.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./style.css";
+
+function Opt({ optLenght = 6 }) {
+  const [valueField, setValueField] = useState(new Array(optLenght).fill(""));
+  const ref = useRef([]);
+
+  const handleKeyDown = (e, index) => {
+    const key = e.key;
+    console.log(key);
+    if (key === "ArrowRight") {
+      if (index + 1 < optLenght) ref.current[index + 1].focus();
+    }
+    if (key === "ArrowLeft") {
+      if (index > 0) ref.current[index - 1].focus();
+    }
+    const copyOtpFields = [...valueField];
+    if (key === "Backspace") {
+      copyOtpFields[index] = "";
+      setValueField(copyOtpFields);
+      if (index > 0) ref.current[index - 1].focus();
+
+      return;
+    }
+    if (isNaN(key)) {
+      return;
+    }
+    copyOtpFields[index] = key;
+    if (index + 1 < optLenght) ref.current[index + 1].focus();
+    setValueField(copyOtpFields);
+  };
+  useEffect(() => {
+    ref.current["0"].focus();
+  }, []);
+  return (
+    <div className="container">
+      {valueField.map((value, index) => {
+        return (
+          <input
+            key={index}
+            ref={(currentInput) => (ref.current[index] = currentInput)}
+            type="text"
+            value={value}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+          ></input>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Opt;
